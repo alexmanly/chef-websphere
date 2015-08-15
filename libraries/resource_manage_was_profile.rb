41f242cc5350ed720eeb8648359c8d2b6d881b26
alexmanly <alexmanly@gmail.com>
2015-08-15 01:07:45 +0100

    file join error

--- libraries/resource_manage_was_profile.rb
+++ libraries/resource_manage_was_profile.rb
@@ -9,22 +9,22 @@ class Chef
       actions :create, :delete, :start, :stop, :wasadmin_single_script, :wsadmin_all_scripts, :install_jdbc_library
       default_action :start
 
-      attribute :install_dir, :name_attribute => true, :kind_of => String
+      attribute :install_dir, :name_attribute => false, :kind_of => String
       attribute :profile_name, :name_attribute => true, :kind_of => String
-      attribute :profile_type, :name_attribute => true, :kind_of => String
-      attribute :node_name, :name_attribute => true, :kind_of => String
-      attribute :cell_name, :name_attribute => true, :kind_of => String
-      attribute :host_name, :name_attribute => true, :kind_of => String
-      attribute :enable_admin_security, :name_attribute => true, :kind_of => String
-      attribute :admin_username, :name_attribute => true, :kind_of => String
-      attribute :admin_password, :name_attribute => true, :kind_of => String
-      attribute :starting_port, :name_attribute => true, :kind_of => String
-      attribute :dmgr_host, :name_attribute => true, :kind_of => String
-      attribute :dmgr_port, :name_attribute => true, :kind_of => String
-      attribute :script_language, :name_attribute => true, :kind_of => String
-      attribute :script_path, :name_attribute => true, :kind_of => String
-      attribute :script_name, :name_attribute => true, :kind_of => String
-      attribute :data, :name_attribute => true, :kind_of => Hash, :default => {}
+      attribute :profile_type, :name_attribute => false, :kind_of => String
+      attribute :node_name, :name_attribute => false, :kind_of => String
+      attribute :cell_name, :name_attribute => false, :kind_of => String
+      attribute :host_name, :name_attribute => false, :kind_of => String
+      attribute :enable_admin_security, :name_attribute => false, :kind_of => String
+      attribute :admin_username, :name_attribute => false, :kind_of => String
+      attribute :admin_password, :name_attribute => false, :kind_of => String
+      attribute :starting_port, :name_attribute => false, :kind_of => String
+      attribute :dmgr_host, :name_attribute => false, :kind_of => String
+      attribute :dmgr_port, :name_attribute => false, :kind_of => String
+      attribute :script_language, :name_attribute => false, :kind_of => String
+      attribute :script_path, :name_attribute => false, :kind_of => String
+      attribute :script_name, :name_attribute => false, :kind_of => String
+      attribute :data, :name_attribute => false, :kind_of => Hash, :default => {}
     end
   end
 end
